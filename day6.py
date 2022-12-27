5d09163927e30b7596f0b45a24387d2eb3ab269b
cuaylkls <72861410+cuaylkls@users.noreply.github.com>
2022-12-27 00:18:46 +0000

    Day 6

---
+++ day6.py
@@ -0,0 +1,33 @@
+with open("day6-input.txt", "r") as f:
+    pos = 4
+    current4 = f.read(4)
+    current14 = current4
+    # cont = True
+
+    while True:
+
+        c = f.read(1)
+
+        if c == '':
+            break
+
+        if len(current4) == len(set(current4)):
+            comm_start = pos
+        else:
+            current4 = current4[1:] + c
+
+        if len(current14) < 14:
+            pass
+        elif len(current14) == len(set(current14)):
+            msg_start = pos
+            break
+
+        pos += 1
+
+        if len(current14) < 14:
+            current14 = current14 + c
+        else:
+            current14 = current14[1:] + c
+
+
+print(msg_start)
