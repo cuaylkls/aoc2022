74e8979e943580b4c52a6d85b4381f16e43ce25f
cuaylkls <72861410+cuaylkls@users.noreply.github.com>
2022-12-29 11:09:21 +0000

    Day 7 - over-engineered but used to practice unittest module use

---
+++ day7-test.py
@@ -0,0 +1,357 @@
+import unittest
+import day7
+
+
+class VFSErrorTestCase(unittest.TestCase):
+    def test_raise(self):
+        def raise_error():
+            raise day7.VFSError("testing")
+
+        # test raise
+        self.assertRaises(
+            day7.VFSError,
+            raise_error
+        )
+
+        # test message
+        try:
+            raise_error()
+        except day7.VFSError as e:
+            self.assertEqual(e.message, "testing")
+
+
+class VFsObjectTestCase(unittest.TestCase):
+    def test_invalid_name_chars(self):
+        """ Test that names line break characters are rejected"""
+        with self.assertRaises(day7.VFSError) as cm:
+            day7.VFsObject("test\n", None)
+
+        e: day7.VFSError = cm.exception
+        self.assertEqual(e.message, "Filesystem object names must not contain newline characters.")
+
+        with self.assertRaises(day7.VFSError) as cm:
+            day7.VFsObject("test\r\n", None)
+
+        e: day7.VFSError = cm.exception
+        self.assertEqual(e.message, "Filesystem object names must not contain newline characters.")
+
+
+class CommandLineTestCase(unittest.TestCase):
+    def test_init(self):
+        self.assertIsInstance(day7.CommandLine(), day7.CommandLine)
+
+    def test_process_command_line(self):
+        def command_order():
+            cmd = day7.CommandLine()
+            cmd.process_command_line("command order test")
+
+        # process a command output before inside a command
+        with self.assertRaises(day7.VFSError) as cm:
+            command_order()
+
+            e: day7.VFSError = cm.exception
+            self.assertEqual(e.message, "Cannot process output outside a command.")
+
+    def test_run_command(self):
+        cmd = day7.CommandLine()
+
+        # running an invalid command raises an exception
+        self.assertRaises(
+            day7.VFSError,
+            lambda: cmd.run_command("unknown 1123")
+        )
+
+    def test_run_command_cd(self):
+        cmd = day7.CommandLine()
+
+        cmd.run_command("cd /")
+        self.assertEqual(cmd.cur_dir.path, "/")
+
+        cmd.run_command("cd test")
+        self.assertEqual(cmd.cur_dir.path, "/test")
+
+        cmd.run_command("cd ..")
+        self.assertEqual(cmd.cur_dir.path, "/", msg="Test cd to parent directory")
+
+        self.assertRaises(
+            NotImplementedError,
+            lambda: cmd.run_command("cd /test/dir/subdir")
+        )
+
+    def test_run_command_ls(self):
+        cmd = day7.CommandLine()
+        cmd.run_command("cd /")
+
+        # check that process is recognised as being inside a command
+        cmd.run_command("ls")
+        self.assertTrue(cmd.in_cmd)
+
+        # check trying to run another command raises an error
+        self.assertRaises(
+            day7.VFSError,
+            lambda: cmd.run_command("ls")
+        )
+
+        # check not implemented is returned
+        del cmd
+        cmd = day7.CommandLine()
+        cmd.run_command("cd /")
+        self.assertRaises(
+            NotImplementedError,
+            lambda: cmd.run_command("ls /some/other/directory")
+        )
+
+        cmd.process_command_line("$ ls")
+        cmd.process_command_line("272080 adam.hdr")
+        cmd.process_command_line("dir fred")
+        cmd.process_command_line("6067 harry")
+
+        files = cmd.file_system.children
+
+        # check types of child items
+        self.assertIsInstance(files[0], day7.VFile)
+        self.assertIsInstance(files[1], day7.VDirectory)
+        self.assertIsInstance(files[2], day7.VFile)
+
+        # check paths are correct
+        self.assertEqual(files[0].path, "/adam.hdr")
+        self.assertEqual(files[1].path, "/fred")
+        self.assertEqual(files[2].path, "/harry")
+
+        # check file sizes
+        self.assertEqual(files[0].size, 272080)
+        self.assertEqual(files[2].size, 6067)
+
+
+    #  test_process_line(self):
+    #    cmd = day7.CommandLine()
+    #    cmd.process_command_line("$ cd /")
+
+
+class VFileTestCase(unittest.TestCase):
+    def test_init(self):
+        class DummyObj(object):
+            pass
+
+        v_dir = day7.VDirectory("/")
+
+        # create with no parent
+        self.assertIsInstance(day7.VFile("name", 0), day7.VFile)
+        self.assertIsInstance(day7.VFile("name", 0,  v_dir), day7.VFile)
+
+        # wrong parent type test
+        self.assertRaises(
+            TypeError,
+            lambda: day7.VFile("name", 0, DummyObj())
+        )
+
+        # size is not an integer
+        self.assertRaises(
+            TypeError,
+            lambda: day7.VFile("name", "s")
+        )
+
+        # size is less than zero
+        self.assertRaises(
+            ValueError,
+            lambda: day7.VFile("name", -1)
+        )
+
+    def test_path(self):
+        v_dir = day7.VDirectory("/")
+        v_file = day7.VFile("my_file", 0, v_dir)
+
+        self.assertEqual(v_file.path, "/my_file")
+
+        v_file2 = day7.VFile("my_file2", 0)
+        self.assertEqual(v_file2.path, "my_file2")
+
+    def test_name(self):
+        v_file = day7.VFile("my_file", 0)
+
+        # test file name
+        self.assertEqual(v_file.name, "my_file")
+
+    def test_parent_dir(self):
+        # no parent
+        self.assertIsNone(day7.VFile("no_parent", 0).parent)
+
+        v_dir = day7.VDirectory("/")
+        v_file = day7.VFile("my_file", 0, v_dir)
+        self.assertEqual(v_file.parent, v_dir)
+
+        # assigning a parent dir
+        v_file2 = day7.VFile("other", 0)
+        v_file2.parent = v_dir
+
+        self.assertTrue(v_file2.parent, v_dir)
+
+        # test removal of parent
+        v_file2.parent = None
+        self.assertIsNone(v_file2.parent)
+
+    def test_size(self):
+        size = 1943
+        v_file = day7.VFile("my_file", size)
+        self.assertEqual(v_file.size, size)
+
+
+class VDirectoryTestCase(unittest.TestCase):
+    @staticmethod
+    def common_setup():
+        # tests init as root v_dir
+        v_dir = day7.VDirectory("/")
+
+        # initilise with a parent directory
+        v_dir2 = day7.VDirectory("subdir", v_dir)
+        v_dir3 = day7.VDirectory("subdir2", v_dir)
+        v_dir4 = day7.VDirectory("no_parent")
+
+        return v_dir, v_dir2, v_dir3, v_dir4
+
+    def test_init(self):
+        class DummyObj(object):
+            pass
+
+        # tests init as root v_dir
+        v_dir = day7.VDirectory("/")
+        self.assertEqual(v_dir.name, "/")
+
+        # initilise with a parent directory
+        v_dir2 = day7.VDirectory("subdir", v_dir)
+        self.assertIsInstance(v_dir2.parent, day7.VDirectory)
+
+        # initilise with the wrong parent_dir type
+        self.assertRaises(
+            TypeError,
+            lambda: day7.VDirectory("subdir", DummyObj)
+        )
+
+    def test_name(self):
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        # test root dir name
+        self.assertEqual(v_dir.name, "/")
+
+        # initilise with a parent directory
+        self.assertEqual(v_dir2.name, "subdir")
+
+    def test_is_root(self):
+        v_dir = day7.VDirectory("/")
+        self.assertTrue(v_dir.is_root)
+
+    def test_path(self):
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        self.assertEqual(v_dir.path, "/")  # root only
+        self.assertEqual(v_dir4.path, "no_parent")  # no parent dir
+        self.assertEqual(v_dir2.path, "/subdir")  # sub directory
+
+        v_dir5 = day7.VDirectory("subdir2", v_dir4)  # parent with no root
+        self.assertEqual(v_dir5.path, "no_parent/subdir2")
+
+    def test_parent_dir(self):
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        # root dir has no parent
+        self.assertIsNone(v_dir.parent)
+
+        # correct parent dir for child
+        self.assertEqual(v_dir2.parent, v_dir)
+
+        # assigning a parent dir
+        v_dir4.parent = v_dir
+        self.assertTrue(v_dir4.parent, v_dir)
+
+        # test removal of parent
+        v_dir3.parent = None
+        self.assertIsNone(v_dir.parent)
+
+    def test_children(self):
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        # no children
+        self.assertEqual(v_dir2.children, [])
+
+        # children
+        self.assertEqual(v_dir.children, [v_dir2, v_dir3])
+
+        # assigning a parent dir
+        v_dir4.parent = v_dir
+        self.assertEqual(v_dir4.parent, v_dir)
+        self.assertEqual(v_dir.children, [v_dir2, v_dir3, v_dir4])
+
+        # removing a parent dir
+        v_dir4.parent = None
+        self.assertEqual(v_dir.children, [v_dir2, v_dir3])
+
+    def test_add_child(self):
+        class DummyObj(object):
+            pass
+
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        # add directory child
+        v_dir.add_child(v_dir4)
+        self.assertEqual(v_dir.children, [v_dir2, v_dir3, v_dir4])
+
+        v_file = day7.VFile("name", 0, v_dir)
+        self.assertEqual(
+            v_dir.children, [v_dir2, v_dir3, v_dir4, v_file]
+        )
+
+        self.assertRaises(
+            TypeError,
+            lambda: v_dir.add_child(DummyObj())
+        )
+
+    def test_add_child_no_duplicates(self):
+        """ Add child check no duplicates """
+
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        def add_duplicate_child():
+            day7.VDirectory("subdir", v_dir)
+
+        with self.assertRaises(day7.VFSError) as cm:
+            add_duplicate_child()
+
+        e: day7.VFSError = cm.exception
+        self.assertTrue(e.message.startswith("Cannot add child with duplicate name"))
+
+    def test_remove_child(self):
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        # remove existing child
+        v_dir.remove_child(v_dir2)
+        self.assertEqual(
+            v_dir.children,
+            [v_dir3, ]
+        )
+        # remove child that does not exists
+        self.assertRaises(
+            ValueError,
+            lambda: v_dir.remove_child(v_dir4)
+        )
+
+    def test_size(self):
+        v_dir, v_dir2, v_dir3, v_dir4 = self.common_setup()
+
+        day7.VFile("file1", 10, v_dir)
+        day7.VFile("file2", 20, v_dir)
+        day7.VFile("file3", 30, v_dir)
+
+        # directory with only file
+        self.assertEqual(v_dir.size, 60)
+
+        # sub directory
+        day7.VFile("file1", 10, v_dir2)
+        day7.VFile("file2", 20, v_dir2)
+        self.assertEqual(v_dir2.size, 30)
+
+        # size with sub-dirs and files
+        self.assertEqual(v_dir.size, 90)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ day7.py
@@ -0,0 +1,299 @@
+import re
+
+
+class VFSError(Exception):
+    @property
+    def message(self) -> str:
+        return self._message
+
+    def __init__(self, message):
+        super(VFSError, self).__init__(message)
+        self._message = message
+
+
+class VFsObject:
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, new_parent):
+        self._set_parent(new_parent)
+
+    @property
+    def path(self):
+        if self._parent is None:
+            return self._name
+        else:
+            if self._parent.is_root:
+                return f"/{self._name}"
+            else:
+                return f"{self._parent.path}/{self._name}"
+
+    def __init__(self, obj_name, parent_dir):
+        if not isinstance(parent_dir, VDirectory) and parent_dir is not None:
+            raise TypeError(f"Invalid type passed to parent_dir argument. "
+                            f"Excepting {VDirectory} got {type(parent_dir)}")
+
+        if re.search("[\r\n]", obj_name) is not None:
+            raise VFSError("Filesystem object names must not contain newline characters.")
+
+        self._name = obj_name
+        self._parent = None
+        self._set_parent(parent_dir)
+
+    def _set_parent(self, new_parent):
+        if (not isinstance(new_parent, VDirectory)) and \
+                new_parent is not None:
+            raise TypeError(f"Invalid type passed to new_parent argument. "
+                            f"Excepting {type(self)} got {type(new_parent)}")
+
+        if self._parent is not None:
+            self._parent.remove_child(self)
+
+        if new_parent is None:
+            self._parent = None
+        else:
+            self._parent = new_parent
+            new_parent.add_child(self)
+
+    def __repr__(self):
+        return f"{type(self).__name__}(\"{self.name}\", {self._parent})"
+
+    def __str__(self):
+        return self.path
+
+
+class VFile(VFsObject):
+
+    @property
+    def size(self):
+        return self._size
+
+    def __init__(self, file_name, size, parent_dir=None):
+        if not isinstance(size, int):
+            raise TypeError(f"Wrong data type for size, expecting int, got {type(size)}")
+
+        if size < 0:
+            raise ValueError("Size must not be a negative number.")
+
+        self._size = size
+
+        super(VFile, self).__init__(file_name, parent_dir)
+
+
+class VDirectory(VFsObject):
+
+    @property
+    def is_root(self):
+        return self._is_root
+
+    @property
+    def children(self):
+        return self._children
+
+    @property
+    def size(self):
+        size = 0
+        for item in self.children:
+            size += item.size
+
+        return size
+
+    def add_child(self, child_obj: VFsObject):
+        if not isinstance(child_obj, VFsObject):
+            raise TypeError(f"Invalid type passed to child_dir argument. "
+                            f"Excepting {type(self)} got {type(child_obj)}")
+
+        for child in self._children:
+            if child.name == child_obj.name:
+                raise VFSError(f"Cannot add child with duplicate name: {child_obj.name}")
+
+        self._children.append(child_obj)
+
+        return child_obj
+
+    def remove_child(self, child_dir):
+        self._children.remove(child_dir)
+
+    def has_child(self, child_name: str) -> bool:
+        for child in self._children:
+            if child.name == child_name:
+                return True
+
+        return False
+
+    def get_child(self, child_name: str) -> VFsObject:
+        for child in self._children:
+            if child.name == child_name:
+                return child
+
+    def __init__(self, dir_name, parent_dir=None):
+        self._children = []
+
+        if dir_name == "/":
+            self._is_root = True
+        else:
+            self._is_root = False
+
+        super(VDirectory, self).__init__(dir_name, parent_dir)
+
+
+class CommandLine:
+    @property
+    def file_system(self):
+        return self._top_dir
+
+    @property
+    def cur_dir(self):
+        return self._cur_dir
+
+    @property
+    def in_cmd(self):
+        return self._in_cmd
+
+    def __init__(self):
+        self._cur_dir: VDirectory = None
+        self._top_dir: VDirectory = None
+        self._in_cmd = False
+        self._output_processor = None
+
+    def _cmd_ls_output_processor(self, output_line):
+        parts = output_line.split(" ")
+        item_name = parts[1].strip()
+
+        if self._cur_dir.has_child(item_name):
+            return
+
+        if parts[0] == "dir":
+            VDirectory(item_name, self._cur_dir)
+        else:
+            VFile(item_name, int(parts[0]), self._cur_dir)
+
+    def _cmd_ls(self, line):
+        if line is not None:
+            raise NotImplementedError("ls for other directories are not currently supported")
+
+        self._in_cmd = True
+        return self._cmd_ls_output_processor
+
+    def _cmd_cd(self, line):
+        arg_dir = line.strip()
+
+        if arg_dir.count("/") > 1 and arg_dir != "/":
+            raise NotImplementedError("Multiple directory cd not yet supported")
+
+        # no forward slashes in path
+        if self._cur_dir is None:
+            self._cur_dir = VDirectory(arg_dir)
+            self._top_dir = self._cur_dir
+        else:
+            if self._cur_dir.has_child(arg_dir):
+                self._cur_dir = self._cur_dir.get_child(arg_dir)
+            else:
+                if arg_dir == "..":
+                    self._cur_dir = self._cur_dir.parent
+                else:
+                    new_dir = VDirectory(arg_dir, self._cur_dir)
+                    self._cur_dir = new_dir
+
+    def run_command(self, command):
+        cmd_funcs = {
+            "cd": self._cmd_cd,
+            "ls": self._cmd_ls,
+        }
+
+        parts = command.split(" ", 2)
+
+        if parts[0] not in cmd_funcs:
+            raise VFSError(f"Command '{parts[0]}' unknown.")
+
+        if self.in_cmd:
+            raise VFSError("Cannot process command output as a command.")
+
+        if len(parts) == 1:
+            parts.append(None)
+
+        self._output_processor = cmd_funcs[parts[0]](parts[1])
+
+    def process_command_line(self, line):
+        if line[0] == "$":
+            self._in_cmd = False
+            self.run_command(line[1:].strip())
+        else:
+            if not self._in_cmd:
+                raise VFSError("Cannot process output outside a command.")
+
+            # if an output processor has been set, process the output, if not, skip
+            if self._output_processor is not None:
+                self._output_processor(line)
+            else:
+                pass
+
+
+def recurse_filesystem(search_dir: VDirectory, max_size=0):
+    child: VDirectory
+
+    size = search_dir.size if search_dir.size <= max_size or max_size == 0 else 0
+
+    for child in search_dir.children:
+        if isinstance(child, VDirectory):
+            size += recurse_filesystem(child, max_size)
+
+    return size
+
+
+def find_smallest(search_dir: VDirectory, min_size, max_size=-1):
+    # scenario 1: directory size is less than min_size, no further search, return None
+    if search_dir.size < min_size:
+        return None
+
+    # scenario 2: directory is greater than or equal to min_size, search each child
+    # directory, if child is greater than min_size, search that directory
+    child: VFsObject
+    smallest = search_dir.size
+
+    for child in search_dir.children:
+        if isinstance(child, VDirectory) and child.size >= min_size:
+            size = find_smallest(child, min_size)
+
+            if size is not None:
+                if size < smallest:
+                    smallest = size
+
+    return smallest
+
+
+def main():
+    cmd = CommandLine()
+
+    with open('day7-input.txt', 'r') as f:
+        for f_line in f:
+            cmd.process_command_line(f_line)
+
+    size = 100000
+    available_space = 70000000
+    target_space = 30000000
+    remaining_space = available_space - cmd.file_system.size
+    space_to_free = target_space - (available_space - cmd.file_system.size)
+
+    print(f"Total available space: {available_space:,}")
+    print(f"Total space used: {cmd.file_system.size:,}")
+    print(f"Remaining space: {remaining_space:,}")
+    print(f"Space required: {target_space:,}")
+    print(f"Space to free: {space_to_free:,}")
+
+    print()
+    print(f"Part 1: {recurse_filesystem(cmd.file_system, size):,}")
+
+    smallest = find_smallest(cmd.file_system, space_to_free)
+
+    print(f"Part 2: {smallest:,}")
+
+if __name__ == '__main__':
+    main()
+
